975b82733221a7c21fbe078cef67dde8d2cfc6ad
Alex Shap[iro <shapira3@wwu.edu>
2026-05-01 17:17:08 -0700

    Update role.java

--- role.java
+++ role.java
@@ -1,11 +1,43 @@
 public class role {
 	
+	private String name;
+    private String quote;
+    private int rank;
+    private boolean occupied = false; //default state false
+    private boolean staringRoll;
+
+    //private boolean reward type? or maybe private enum reward type with starting + extra? idk if reward increase is static, check later
+
+
+	public role(String name, String quote, int rank, boolean occupied, boolean staringRoll){
+
+        this.name = name;
+        this.quote = quote;
+        this.rank = rank;
+        this.occupied = occupied;
+        this.staringRoll = staringRoll;
+    }
 	
-	
-	
-	
-	
-	
-	
-	
+	public String getName(){
+        return name;
+    }
+    public String getQuote(){
+        return quote;
+    }
+	public int getRank(){
+        return rank;
+    }
+	public boolean getOccupied(){
+        return occupied;
+    }
+	public boolean getStarringRoll(){
+        return staringRoll;
+    }
+    public void setOccupied(boolean x){
+        if (x = true){
+            this.occupied = true;
+        } else{
+            this.occupied=false;
+        }
+    }
 }
