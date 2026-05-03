f01ecedd8bb4a73fedfdc7855c97dd1d43051d20
kennyp83 <ken.805@yahoo.com>
2026-05-03 10:26:05 -0700

    gameOver partially implenmented

--- gameOverManager.java
+++ gameOverManager.java
@@ -1,4 +1,15 @@
 public class gameOverManager{
 	
+	public boolean isGameOver(int day, int playerCount) {
+		if (playerCount < 4) {
+			return day > 3;
+		}
+		return day > 4;
+	}
 	
+	public void endGame(int day, int playerCount) {
+		if (isGameOver(day, playerCount)) {
+			System.out.println("THE GAME IS OVER"); // needs real end game logic
+		}
+	}
 }
