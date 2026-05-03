b0bae15fea3b317f058a8d80d51940b56f8c016e
kennyp83 <ken.805@yahoo.com>
2026-05-03 09:50:36 -0700

    Added SceneDeck skeleton

--- sceneDeck.java
+++ sceneDeck.java
@@ -1,4 +1,36 @@
+import java.util.ArrayList;
+import java.util.Collections;
+
+import javax.smartcardio.Card;
+
 public class sceneDeck{
+	private ArrayList<Card> deck;
+	
+	public sceneDeck() {
+		deck = new ArrayList<Card>();
+	}
+
+	public void shuffleDeck() {
+		Collections.shuffle(deck);
+	}
+	
+	public Card drawCaard() {
+		if (deck.isEmpty()) {
+			System.out.print("The deck is empty.");
+			return null;
+		}
+		return deck.remove(0);
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
 	
 	
 }
