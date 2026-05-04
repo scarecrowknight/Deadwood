51a310ca06ecca2d9ac83cf43090a07598b9ee09
Alex Shap[iro <shapira3@wwu.edu>
2026-05-04 16:51:43 -0700

    deleted scene (duplicate of room). Major Edits to Set.java

--- Set.java
+++ Set.java
@@ -1,22 +1,42 @@
+import java.lang.reflect.Array;
 import java.util.*;
 public class Set extends Room {
 
-	public String sceneName;
-	public int shotCount; // xml calls these takes
-	public int currentPlayers;
+	private int shotCount = 0; // xml calls these takes
+	private int currentPlayers = 0;
 	/*the array lines can hold the line for each role "Well, I'll be!",
 	also its size is the max number of roles for the scene */
-	public ArrayList<String> lines; 
-	public int maxRoles;
+	private List<role> roles = null;
+	private card activeCard = null;
 	
-   public Set(String sceneName,String name, Board board, int shotCount, int currentPlayers, ArrayList<String> lines) {
+   public Set(String name, Board board, int shotCount, int currentPlayers, card activeCard) {
 	   super(name, board);
-	   this.sceneName = sceneName;
-	   this.shotCount = shotCount;
-	   this.currentPlayers = currentPlayers;
-	   this.lines = lines;
-	   this.maxRoles = lines.size();
+	   this.shotCount = shotCount; //contains shots to be applied by board manager
+	   this.currentPlayers = currentPlayers; //list of current players on set
+	   this.roles = roles; //containes the the "static roles" to be appended by parser
+	   this.activeCard = activeCard; //contains active card on the set to be applied by board manager
    }
+   public card getActiveCard() {
+	   return this.activeCard;
+   }
+   public void setActiveCard(card activeCard) {
+	   this.activeCard = activeCard;
+   }
+
+   public int getShotCount() {
+	   return this.shotCount;
+   }
+   public void setShotCount(int shotCount) {
+	   this.shotCount = shotCount;
+   }
+
+   public int getCurrentPlayers() {
+	   return this.currentPlayers;
+   }
+   public void setCurrentPlayers(int currentPlayers) {
+	   this.currentPlayers = currentPlayers;
+   }
+
    //THIS IS INCOMPLETE AND LITERALLY JUST FOR LETTING MY XML PARSER COMPILE DELETE LATER
    public Set(String name, Board board){
       super(name, board);
