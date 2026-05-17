bb841120cf1f8e8c49e8ee3fb23f83cb584a21b7
Alex Shap[iro <shapira3@wwu.edu>
2026-05-17 00:26:57 -0700

    turned kendal's take role into independent class

---
+++ TakeRoleManager.java
@@ -0,0 +1,70 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class TakeRoleManager {
+    private Board board;
+    private View view;
+
+    public TakeRoleManager(Board board, View view) {
+        this.board = board;
+        this.view = view;
+    }
+    public void TakeRole(Player currentPlayer) {
+                // take role: Scans current set of unoccupied off card and on card roles. Filters
+            	//            out roles player cant take. If a valid role is picked,
+            	//            role is assigned to player, marked occupied, and turn ends
+                Room loc = currentPlayer.currentLocation();
+                Set currentSet = (Set) loc;
+                List<Role> availableRoles = new ArrayList<>();
+                List<String> roleNames = new ArrayList<>();
+                
+                // get all unoccupied off card roles the player has rank for
+                if(currentSet.getOnCardroles() != null) {
+                	for(Role r : currentSet.getOnCardroles()) {
+                		if(!r.getOccupied() && currentPlayer.getRank()>= r.getRank()) {
+                			availableRoles.add(r);
+                			roleNames.add(r.getName());
+                		}
+                	}
+                }
+                //if no roles for players current rank
+                if(availableRoles.isEmpty()) {
+                	view.showMessage("Your rank is too low for that.");
+                	return;
+                }
+                
+                roleNames.add("Cancel");
+                
+                //Query for the user's wanted role
+                Packet roleQuery = new Packet(currentPlayer, loc, board, roleNames, Packet.EventType.QUERY_TAKE_ROLE);
+                String roleChoice = view.renderAndRequestAction(roleQuery);
+                
+                if(roleChoice.equalsIgnoreCase("cancel")) {
+                	return;
+                }
+                
+                //find the role they typed
+                Role chosenR = null;
+                for (Role r : availableRoles) {
+                	if (r.getName().equalsIgnoreCase(roleChoice)) {
+                		chosenR = r;
+                		break;
+                	}
+                }
+                
+                if(chosenR != null) {
+                	currentPlayer.setRole(chosenR);
+                	chosenR.setOccupied(true);
+                	//role taken = turn over.
+                	//turnComplete = true;
+                	
+                	Packet tookRole = new Packet(currentPlayer, loc, board, null, Packet.EventType.TOOK_ROLE);
+                	tookRole.setRoleData(chosenR);
+                	view.render(tookRole);
+                } else {
+                	Packet invalid = new Packet(currentPlayer, loc, board, null, Packet.EventType.INVALID_ACTION);
+                	view.render(invalid);
+                }
+            }
+        }
+        
