eb63da268574042ce9386ea689b2eea3b7084462
scarecrowknight <siri.sk.khalsa@gmail.com>
2026-06-05 15:40:57 -0700

    Pushed trailer class

---
+++ Trailer.java
@@ -0,0 +1,6 @@
+public class Trailer extends Room {
+   //since Trailer is a different shape could extend to add unique Area properties locally
+   public Trailer(String name, Board board){
+      super(name, board);
+   }
+}
